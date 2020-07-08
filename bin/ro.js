4b6233c93b14219dfcb087979e79810625d54bc2
wb_luozhaochang <zhaochang.luo@zatech.com>
2020-07-08 15:03:10 +0800

    update
    This reverts commit 1d88145826f6c87ccc7b34b7b6075e3dbbaafa9e.

--- bin/ro.js
+++ bin/ro.js
@@ -1,8 +1,3 @@
 #!/usr/bin/env node
 
-const program = require('commander')
-
-program.command('init').description('初始化项目').alias('i').action(() => {
-    console.log('--op-->')
-})
-console.log('--op-->')
+require('../src/e-init/index.js')
