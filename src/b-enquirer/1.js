a1c5a2f82c3140706f00cef3f1b1b87606bb8662
roaiKim <shcroai@gmail.com>
2020-07-08 23:35:22 +0800

    init

--- src/b-enquirer/1.js
+++ src/b-enquirer/1.js
@@ -16,8 +16,18 @@ const { prompt } = require('enquirer')
 
 enq() */
 
-prompt({
+/* prompt({
     type: 'confirm',
     name: 'question',
-    message: 'Did you like enquirer'
+    message: 'Did you like enquirer',
+    skip: false,
+    initial: 'ros'
+}).then(answer => console.log('Answer: ', answer))
+ */
+
+prompt({
+    type: 'select',
+    name: 'question',
+    message: 'Did you like enquirer',
+    choices: ['apple', 'grape', 'watermelon', 'cherry', 'orange']
 }).then(answer => console.log('Answer: ', answer))
--- src/b-enquirer/2.js
+++ src/b-enquirer/2.js
@@ -1,13 +1,33 @@
-const { Confirm, Input } = require('enquirer')
+const { Confirm, Input, MultiSelect, Select } = require('enquirer')
 
 /* const prompt = new Confirm({
   name: 'question',
   message: 'Did you like enquirer?'
 }); */
 
-const prompt = new Input({
+/* const prompt = new Input({
   name: 'username',
   message: 'what is your username'
-})
+}) */
+
+/* const prompt = new MultiSelect({
+  name: 'value',
+  message: 'Pick your favorite colors',
+  limit: 0,
+  choices: [
+    { name: 'aqua', value: '#00ffff' },
+    { name: 'black', value: '#000000' },
+    { name: 'blue', value: '#0000ff' },
+    { name: 'fuchsia', value: '#ff00ff' },
+    { name: 'gray', value: '#808080' },
+    { name: 'green', value: '#008000' },
+  ]
+}) */
+
+const prompt = new Select({
+  name: 'color',
+  message: 'Pick a flavor',
+  choices: ['apple', 'grape', 'watermelon', 'cherry', 'orange']
+});
 
 prompt.run().then(answer => console.log('Answer: ', answer))
