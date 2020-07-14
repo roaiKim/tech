03f3f4ec6b83814a3ab32afa10709d9c2d8e4606
wb_luozhaochang <zhaochang.luo@zatech.com>
2020-07-14 19:26:32 +0800

    update

--- src/e-init/cmd/create.js
+++ src/e-init/cmd/create.js
@@ -1,14 +1,9 @@
 const path = require('path')
 const download = require('download-git-repo')
 const ora = require('ora')
+const chalk = require('chalk')
 
-const spinner = ora({
-    text: '\ndownloading template, please wait...', // downloading template, please wait...
-    spinner: {
-        interval: 260,
-        frames: [".", "...", "....", ".....", "......"]
-    }
-})
+const spinner = ora('downloading template, please wait...')
 
 module.exports = (projectName) => {
     /* console.log(`此处是文件 ${projectName.join()}`)
@@ -16,8 +11,23 @@ module.exports = (projectName) => {
     console.log(`__dirname: ${__dirname}`)
     console.log(`__filename: ${__filename}`)
     console.log(`path.resolve: ${path.resolve('./')}`) */
-    spinner.start()
-    download('github:roaiKim/react-ro', path.join(process.cwd(), projectName[0]), (error) => {
+
+    console.log('projectName-->', projectName)
+    if (!projectName[0]) {
+        console.log(chalk.red('ro create 的第一个参数为必填项 \nro create <dir-name> [options]'))
+        process.exit(-1);
+    }
+    if (projectName[0].startsWith("--")) {
+        console.log(chalk.red('ro create 的第一个参数为文件夹名字, 不是参数 \nro create <dir-name> [options]'))
+        process.exit(-1);
+    }
+    const dirName = projectName[0];
+    projectName.shift()
+    const optObj = projectName.map(_ => _.startsWith("--") ? _ + "#" : _).join(",").split(/--/g).slice(1).filter(_ => _).reduce((a, b) => (a[b.split('#')[0]] = (b.split('#')[1].split(",").filter(_ => _).join(",")), a), {})
+    console.log('optObj-->', optObj)
+    // spinner.start()
+
+    /* download('github:roaiKim/react-ro', path.join(process.cwd(), projectName[0]), (error) => {
         if (error) {
             console.error('拉取仓库错误')
             spinner.stop();
@@ -25,5 +35,5 @@ module.exports = (projectName) => {
         }
         spinner.stop();
         console.log('成功!')
-    })
+    }) */
 }
--- src/f-init2/index.js
+++ src/f-init2/index.js
@@ -3,12 +3,13 @@ const {version} = require('./util')
 const {mapCommand} = require('./cmd/options.js')
 const path = require('path')
 
-Object.entries(mapCommand).forEach(([action, cmd]) => {
+/* Object.entries(mapCommand).forEach(([action, cmd]) => {
     program.command(action).alias(cmd.alias).description(cmd.description).allowUnknownOption()
     .action(() => {
         if(action === "*") {
             console.log(cmd.description)
         } else {
+            console.log('program.name', program.name(), process.argv)
             require(path.join(__dirname, 'cmd', action))([...process.argv.slice(3)])
         }
     })
@@ -24,7 +25,13 @@ program.on('--help', () => {
 })
 
 program.version(version)
-       .parse(process.argv);
+       .parse(process.argv); */
 
+program.command("init").alias("i").description("初始化...")
+.option('-f, --float <number>', 'float')
+.allowUnknownOption()
+.parse(process.argv);
 
+const {float} = program
 
+console.log('float: ' + float)
